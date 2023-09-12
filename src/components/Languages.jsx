fa0842ff0cbc81c8279c4b9d7a0ef6f77527d29a
ifeoluwa <ifeoluwaamidah@gmail.com>
2023-09-12 11:42:36 +0100

    done

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -8,7 +8,7 @@ export default function Example() {
         <div className="bg-black pyhy`-24 sm:py-24">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <h2 className="text-center text-lg font-bold leading-8 text-gray-500">
-                    Some of the programming languages i know
+                    My area of specialization
                 </h2>
                 <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                     <div className="flex items-center col-span-2 max-h-12 w-full object-contain lg:col-span-1">
